ea413c26b5d876c7c848e0042d49626b951171c9
NaveenSPrabhu <NaveenSPrabhu@users.noreply.github.com>
2026-04-24 06:00:06 +0530

    Fix CORS for mobile access

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ app = FastAPI(
     version="1.0.0"
 )
 
-# Add CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
